f81c2004ce2fbf9ec2772925ee281965c98ad4c3
Kellen Miller <kellen.miller@finxact.com>
2023-08-29 15:34:58 -0400

    zap logger for watermill

--- go/go.mod
+++ go/go.mod
@@ -3,3 +3,12 @@ module github.com/kellen-miller/advent-of-code/go
 go 1.20
 
 require github.com/ugurcsen/gods-generic v0.10.2
+
+require (
+	github.com/ThreeDotsLabs/watermill v1.3.3 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/lithammer/shortuuid/v3 v3.0.7 // indirect
+	github.com/oklog/ulid v1.3.1 // indirect
+	go.uber.org/multierr v1.10.0 // indirect
+	go.uber.org/zap v1.25.0 // indirect
+)
---
+++ go/watermill/logger.go
@@ -0,0 +1,65 @@
+package watermill
+
+import (
+	"github.com/ThreeDotsLabs/watermill"
+	"go.uber.org/zap"
+)
+
+// Logger implements watermill.LoggerAdapter with *zap.Logger.
+type Logger struct {
+	base      *zap.Logger
+	logFields watermill.LogFields
+}
+
+// NewLogger returns new watermill.LoggerAdapter using passed *zap.Logger as base.
+func NewLogger(z *zap.Logger) watermill.LoggerAdapter {
+	return &Logger{base: z}
+}
+
+// Error writes error log with message, error and some logFields.
+func (l *Logger) Error(msg string, err error, wmFields watermill.LogFields) {
+	zapFields := l.convFields(wmFields)
+	zapFields = append(zapFields, zap.Error(err))
+
+	l.base.Error(msg, zapFields...)
+}
+
+// Info writes info log with message and some logFields.
+func (l *Logger) Info(msg string, fields watermill.LogFields) {
+	l.base.Info(msg, l.convFields(fields)...)
+}
+
+// Debug writes debug log with message and some logFields.
+func (l *Logger) Debug(msg string, fields watermill.LogFields) {
+	l.base.Debug(msg, l.convFields(fields)...)
+}
+
+// Trace writes debug log instead of trace log because zap does not support trace level logging.
+func (l *Logger) Trace(msg string, fields watermill.LogFields) {
+	l.base.Debug(msg, l.convFields(fields)...)
+}
+
+// With returns new LoggerAdapter with passed logFields.
+func (l *Logger) With(fields watermill.LogFields) watermill.LoggerAdapter {
+	return &Logger{
+		base:      l.base,
+		logFields: l.logFields.Add(fields),
+	}
+}
+
+func (l *Logger) convFields(logFields watermill.LogFields) []zap.Field {
+	zfs := make([]zap.Field, len(l.logFields)+len(logFields))
+	i := 0
+
+	for k, v := range l.logFields {
+		zfs[i] = zap.Any(k, v)
+		i++
+	}
+
+	for k, v := range logFields {
+		zfs[i] = zap.Any(k, v)
+		i++
+	}
+
+	return zfs
+}
